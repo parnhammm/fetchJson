6a056f4f872c6e5db36b6c8ca1f0831a4db73e41
Andrew Parnham <atparnham@gmail.com>
2019-12-16 13:33:24 +0000

    Refining the program with an interface

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var urlToRequest = "http://jsonplaceholder.typicode.com/todos/1";
 //axios.get() will return a promise, so lets use the then to print out the response
-axios_1["default"].get(urlToRequest).then(function (response) {
-    console.log(response.data);
+axios_1["default"].get(urlToRequest).then(function(response) {
+  const todo = response.data;
+
+  const ID = todo.ID,
+    title = todo.Title,
+    finished = todo.Finished;
+
+  console.log(`
+        The Todo with ID: ${ID} 
+        Has a title of ${title}
+        Is it finished? ${finished}
+    `);
 });
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,21 @@ import axios from "axios";
 
 const urlToRequest = "http://jsonplaceholder.typicode.com/todos/1";
 
+//Interfaces in typescript are used to define the structure of an object
+//Here we are defining what the resulting call to this API will produce
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 //axios.get() will return a promise, so lets use the then to print out the response
 axios.get(urlToRequest).then(response => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  console.log(`
+    The Todo with an ID: ${todo.id}
+    Has the title: ${todo.title}
+    Is completed? ${todo.completed}
+  `);
 });
